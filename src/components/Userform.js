decd7d8a678b8f0a400c7f1491131bdcc620f767
Usama <jusamaoct@gmail.com>
2022-06-20 18:24:51 +0500

    Functionality to AddTask Underway

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,28 +1,52 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import "./style.css";
 
 const Userform = ({ btnText, onclick }) => {
+  const {text, setText} = useState('');
+  const {day, setDay} = useState('');
+  const {reminder, setReminder} = useState(false);
+
   return (
     <div>
       <div className="container my-3">
         <form action="" id="my-form" className="shadow p-5">
           <h5>Add Task</h5>
-          <div>
+          <div className="form-control border-0">
             <input
               type="text"
               placeholder="Task"
-              className="w-100 pt-4 my-1 border-0 border-bottom"
+              className="w-100 pt-4 border-0 border-bottom"
+              value={text}
+              onChange={(e)=> setText(e.target.value)}
             />
           </div>
-          <div>
+          <div className="form-control border-0">
             <input
               type="text"
-              placeholder="Time"
-              className="w-100 pt-4 my-1 border-0 border-bottom"
+              placeholder="Date & Time"
+              className="w-100 pt-4 border-0 border-bottom"
+              value={day}
+              onChange={(e)=> setDay(e.target.value)}
+            />
+          </div>
+          <div className="form-check form-switch d-flex justify-content-between p-0 mx-3 my-4">
+            <label
+              className="form-check-label text-secondary"
+              htmlFor="reminderSwitch"
+            >
+              Reminder
+            </label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="reminderSwitch"
+              value={reminder}
+              onChange={(e)=> setReminder(e.currentTarget.checked)}
             />
           </div>
           <div>
-            <button onClick={onclick} className="btn btn-dark w-100 mt-4">
+            <button onClick={onclick} className="btn btn-dark w-100">
               {btnText}
             </button>
           </div>
