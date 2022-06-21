70ed6c906f7cce4b94d07e8a83afee702e017a75
Usama <jusamaoct@gmail.com>
2022-06-21 12:26:12 +0500

    Tasks Complete - Server Remains

--- src/App.js
+++ src/App.js
@@ -4,15 +4,34 @@ import Userform from "./components/Userform";
 import Tasks from "./components/Tasks";
 
 const App = () => {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState([
+    // {
+    //   id: 1,
+    //   text: "Doctors Appointment",
+    //   day: "Feb 5th at 2:30pm",
+    //   reminder: true,
+    // },
+    // {
+    //   id: 2,
+    //   text: "Meeting at School",
+    //   day: "Feb 6th at 1:30pm",
+    //   reminder: true,
+    // },
+    // {
+    //   id: 3,
+    //   text: "Food Shopping",
+    //   day: "Feb 5th at 2:30pm",
+    //   reminder: false,
+    // },
+  ]);
 
   // Add Task
-  const addTask = (e) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTodo = { id, ...e };
-    setTodo([...todo, newTodo]);
-  };
-
+  const addTask = (e)=>{
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTodo = {id, ...e}
+    setTodo([...todo, newTodo])
+  }
+  
   // Delete Task
   const deleteTask = (id) => {
     setTodo(todo.filter((eachtask) => eachtask.id !== id));
@@ -31,7 +50,7 @@ const App = () => {
   return (
     <div className="App">
       <Header title="React-Shinobi" />
-      <Userform btnText="Submit" onAdd={addTask} />
+      <Userform btnText="Submit" onAdd={addTask}/>
       <Tasks todoit={todo} onDelete={deleteTask} onToggle={toggleReminder} />
     </div>
   );
